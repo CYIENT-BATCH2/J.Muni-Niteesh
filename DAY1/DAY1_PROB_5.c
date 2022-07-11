4f6678bdb1756d53aed615bd235339b6d47814ac
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-11 16:44:50 +0530

    Create DAY1_PROB_5.c

---
+++ DAY1/DAY1_PROB_5.c
@@ -0,0 +1,14 @@
+1)include the header file
+2)read the digit from the user
+3)write the condition for divide each number 2 
+4)print the output
+{
+     int n;
+     printf("enter the digit \n");
+     scanf("%d",&n);
+    if(n%2==0)
+    printf("given number is even %d\n",n);
+    else
+    printf("given number is odd %d\n",n);
+    return 0;
+}
