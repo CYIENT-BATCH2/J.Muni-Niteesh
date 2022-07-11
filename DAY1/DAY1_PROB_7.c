8d3f76d0078565c0b6e3d17e0378aa276706e75c
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-11 16:48:31 +0530

    Create DAY1_PROB_7.c

---
+++ DAY1/DAY1_PROB_7.c
@@ -0,0 +1,21 @@
+1)include the header file
+2)read the digit from the user
+3)write the condition for divide each number and check whether it is polindrome or not
+4)print the output
+int n,j,k=0,m;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  m=n;
+  loop:if(n>0)
+{
+      j=n%10;
+      k=k*10+j;
+      n=n/10;
+  
+  goto loop;
+ }
+ if(m==k)
+ printf("number is polindrome %d\n",k);
+ else
+ printf("number not is polindrome %d\n");
+    return 0;
