3f49698c7fa60abb5c9ce93c49ff927c1968c10b
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-11 16:42:50 +0530

    Create DAY1_PROB_3.c

---
+++ DAY1/DAY1_PROB_3.c
@@ -0,0 +1,13 @@
+1)include the header file
+2)read the radius from the user
+3)write the area of the circular garden 
+4)print the area output 
+#include <stdio.h>
+int main() {
+int r,c;
+     printf("enter the radius\n");
+     scanf("%d",&r);
+     c=3.142*r*r;
+     printf("area of the garden is %d\n",c);
+    return 0;
+}
