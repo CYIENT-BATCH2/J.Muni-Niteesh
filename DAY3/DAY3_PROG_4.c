15e0090bd84f6fa28b246701055e4c0548ce8f44
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-12 21:33:07 +0530

    Create DAY3_PROG_4.c

---
+++ DAY3/DAY3_PROG_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+  int r,n,k=0,m,j=7;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  m=n;
+  loop:
+  if(n!=0)
+  {
+      r=n%16;
+      if(r<10)
+      r=48+r;
+      else
+      r=55+r;
+      k=k*100+r;
+      printf("%c",r);
+      n=n/16;
+      goto loop;
+  }
+    lop:
+     if(j>=0&&j<8){
+      printf("%d",m>>j&1);
+     j--;
+  goto lop;}
+    return 0;
+}
