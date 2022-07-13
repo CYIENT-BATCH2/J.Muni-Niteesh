8f0990fe6834f29c324ff7367201270bd0a53859
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 12:34:45 +0530

    Update DAY3_PROG_7.c

--- DAY3/DAY3_PROG_7.c
+++ DAY3/DAY3_PROG_7.c
@@ -1,3 +1,8 @@
+/*1)include the header file
+2)read the year and month from the user
+3)write the substract with current year and month calculate age 
+4)print the output
+*/
 #include <stdio.h>
 int main()
 {
