caa33330f673e5ed2e26a38236f24a5fb6b0a67c
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 12:11:52 +0530

    Update DAY2_PROG_6.c

--- DAY2/DAY2_PROG_6.c
+++ DAY2/DAY2_PROG_6.c
@@ -1,3 +1,7 @@
+/*1)include the header file
+2)read the degree and Fahrenheit from the user
+3)write the condition to find degree and Fahrenheit
+4)print the output */
 #include <stdio.h>
 int main() {
     float f,d;
