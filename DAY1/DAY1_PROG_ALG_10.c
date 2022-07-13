16ff4a6032059cc778f54dc4f67da6f5852d5d64
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 11:46:20 +0530

    Update and rename DAY1_PROB_10.c to DAY1_PROG_ALG_10.c

--- DAY1/DAY1_PROG_ALG_10.c
+++ DAY1/DAY1_PROG_ALG_10.c
@@ -1,8 +1,8 @@
-1)include the header file
+/*1)include the header file
 2)read the digit from the user
 3)write the condition by checking each other numbers
 4)print the output
-
+*/
 int n,n1,n2;{
 if(n>n1 && n>n2)
 printf("%d is greater than other two",n);
