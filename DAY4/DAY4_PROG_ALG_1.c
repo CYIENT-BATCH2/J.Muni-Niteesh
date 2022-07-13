14e9dd88a967289e2c9b4aa54ef812e9ca295aaa
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 15:35:56 +0530

    Update and rename DAY4_PROB_1.c to DAY4_PROG_ALG_1.c

--- DAY4/DAY4_PROG_ALG_1.c
+++ DAY4/DAY4_PROG_ALG_1.c
@@ -1,4 +1,8 @@
-(A)ARITHMATICAL OPERATOR
+/*(A)ARITHMATICAL OPERATOR
+1)write the expression from the question
+2)calculate the expression
+3)print the output
+*/
 #include <stdio.h>
 int main() {
     int x=30,y=20,z=10,k;
