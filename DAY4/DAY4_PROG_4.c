bd099037a880445e066c33e132917dc8d69c24f1
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 16:11:24 +0530

    Update DAY4_PROG_4.c

--- DAY4/DAY4_PROG_4.c
+++ DAY4/DAY4_PROG_4.c
@@ -1,4 +1,8 @@
-(D)BITWISE OPERATOR
+/*(D)BITWISE OPERATOR
+1)write the expression from the question
+2)calculate the expression
+3)print the output
+*/
 #include <stdio.h>
 int main() {
       // int i=10,j=20,m=5,k;
