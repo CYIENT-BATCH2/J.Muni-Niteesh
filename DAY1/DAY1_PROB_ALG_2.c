0951e9c25628577ddd5617b5ea934a0cab439d43
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 10:37:58 +0530

    Update and rename DAY1_PROB_2.c to DAY1_PROB_ALG_2.c

--- DAY1/DAY1_PROB_ALG_2.c
+++ DAY1/DAY1_PROB_ALG_2.c
@@ -1,3 +1,7 @@
+/*1)include the header file
+2)read the character from the user
+3)write the condition to check whether it is vowel or consonant
+4)print the output*/
 #include <stdio.h>
 int main() {
 char c;
