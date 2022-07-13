cedeef0c156ef9da2905c86bc2897acafde78504
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 11:44:23 +0530

    Update and rename DAY1_PROB_8.c to DAY1_PROG_ALG_8.c

--- DAY1/DAY1_PROG_ALG_8.c
+++ DAY1/DAY1_PROG_ALG_8.c
@@ -1,7 +1,11 @@
-1)include the header file
+/*1)include the header file
 2)read the digit from the user
 3)print the output with %d format specifier 
-
+*/
+#include<stdio.h>
+void main()
+{
 char c;
 scanf("%c",&c);
 printf("%d",c);
+}
