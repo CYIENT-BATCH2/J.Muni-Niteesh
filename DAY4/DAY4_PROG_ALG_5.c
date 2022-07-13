6c8f32f523f3267cf6f7eb999755c3e818bbae06
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2022-07-13 16:12:35 +0530

    Update and rename DAY4_PROG_5.c to DAY4_PROG_ALG_5.c

--- DAY4/DAY4_PROG_ALG_5.c
+++ DAY4/DAY4_PROG_ALG_5.c
@@ -1,4 +1,8 @@
-(E)INCREMENT AND DECREMENT
+/*(E)INCREMENT AND DECREMENT
+1)write the expression from the question
+2)calculate the expression
+3)print the output
+*/
 #include <stdio.h>
 int main() {
    int i=10,j=50,k;
